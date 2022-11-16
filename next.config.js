7133653b4c07afb5a77267250e2297c5c8a20be2
fhkathika <fhkathika057@gmail.com>
2022-11-16 21:15:58 +0600

    fixing deploy issue

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
 
 module.exports = nextConfig,
 module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   experimental: {
     forceSwcTransforms: true,
   },
